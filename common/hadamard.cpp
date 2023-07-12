ea9e0ce0b229118cf0177e6d22f98df9faa7a13b
qt-bb <karpelevi4@gmail.com>
2023-07-12 11:51:12 +0300

    Общие исходники

---
+++ common/hadamard.cpp
@@ -0,0 +1,41 @@
+#include "./nmtype.h"
+#include "nmpp.h"
+
+/*size=32, time=2.843628
+size=64, time=11.207092
+size=128, time=46.435730 c++ transp
+
+
+size=32, time=1.011292
+size=64, time=2.287170
+size=128, time=9.574402 fast
+
+size=32, time=447.391479
+size=64, time=3559.143860
+size=128, time=28408.257996 c++ mulmm
+
+size=32, time=449.230347
+size=64, time=3568.077026
+size=128, time=28445.145386 slow*/
+void nmppsHadamard(nm32s* src, nm32s* dst, nm2s* H, nm32s* temp, int size) {
+	// dst = H * (H * src^T)^T
+
+	nmppsTranspose(src, temp, size, size);
+	nmppsMulMM_2s32s_W2(H, size, size, temp, dst, size);
+	nmppsTranspose(dst, temp, size, size);
+	nmppsMulMM_2s32s_W2(H, size, size, temp, dst, size);
+}
+
+void nmppsHadamardInverse(nm32s* src, nm32s* dst, nm2s* H, nm32s* temp, int size) {
+	// dst = (1/n * H *((1/n * H * src)^T))^T
+
+	int nShift = 0;
+	while (size >>= 1) nShift++; 
+
+	nmppsMulMM_2s32s_W2(H, size, size, src, temp, size);
+	nmppsRShiftC_32s(temp, nShift, dst, size*size);
+	nmppsTranspose(dst, temp, size, size);
+	nmppsMulMM_2s32s_W2(H, size, size, temp, dst, size);
+	nmppsRShiftC_32s(dst, nShift, temp, size*size);
+	nmppsTranspose(temp, dst, size, size);
+}
---
+++ common/mulmm.cpp
@@ -0,0 +1,13 @@
+#include "./nmtype.h"
+#include "../include/hadamard.h"
+
+void nmppsMulMM_2s32s(nm2s* A, int H, int W1, nm32s* B, nm32s* C, int W2) {
+	nm32s * src_col = B;
+	nm32s * dst_col = C;
+		
+	for(int i = 0; i < W2; i += 2) {
+		nmppsMulMM_2s32s_2col(A, H, W1, src_col, dst_col, W2);
+		src_col+=2;
+		dst_col+=2;
+	}
+}
