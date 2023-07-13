8e878b7b523fd7e033d11de3467df9782ec23759
qt-bb <karpelevi4@gmail.com>
2023-07-13 12:26:00 +0300

    hadamard matrix initialization test

--- test/hadamard_init/main.cpp
+++ test/hadamard_init/main.cpp
@@ -1,12 +1,16 @@
-#include "stdio.h"
-#include "time.h"
-#include "string.h"
-#include "stdlib.h"
 #include "../../include/nmtype.h"
 #include "../include/hadamard.h"
 
+const int size_had = 512;
+__attribute__((section(".data.imu0"))) long long H2[size_had*(size_had/32)];
+
 int main() {
+	int hash = 0;
 
+	for (int i = 32; i <= size_had; i<<=1) {
+		nmppsHadamardInit((nm2s*) H2, i);
+		hash ^= nmppsHash64u((long long *) H2, i*i/32);
+	}
 
-	return 0;
+	return hash^0xf7939d8e;
 } 
