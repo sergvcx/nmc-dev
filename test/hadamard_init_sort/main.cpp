de604440ccb4439dc6c45b2f48ebbba77a3ce36d
qt-bb <karpelevi4@gmail.com>
2023-07-13 15:05:38 +0300

    nm qemu init sort test

--- test/hadamard_init_sort/main.cpp
+++ test/hadamard_init_sort/main.cpp
@@ -1,12 +1,18 @@
-#include "stdio.h"
-#include "time.h"
-#include "string.h"
-#include "stdlib.h"
 #include "../../include/nmtype.h"
 #include "../include/hadamard.h"
 
+const int size_had = 512;
+__attribute__((section(".data.imu0"))) long long H2[size_had*(size_had/32)];
+__attribute__((section(".data.imu1"))) long long H2_sort[size_had*(size_had/32)];
+
 int main() {
+	int hash = 0;
 
+	for (int i = 32; i <= size_had; i<<=1) {
+		nmppsHadamardInit((nm2s*) H2, i);
+		nmppsHadamardInitSort((nm2s*) H2,(nm2s*) H2_sort, i);
+		hash ^= nmppsHash64u((long long *) H2_sort, i*i/32);
+	}
 
-	return 0;
+	return hash^0xf32dfdb8;
 } 
