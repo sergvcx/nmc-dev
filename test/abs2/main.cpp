4df37a94fc6db0983b169cc9760b8059c4ef8a17
qt-bb <karpelevi4@gmail.com>
2023-07-17 17:27:50 +0300

    nm qemu pc abs2 succesful test

--- test/abs2/main.cpp
+++ test/abs2/main.cpp
@@ -1,4 +1,5 @@
 #include "stdlib.h"
+#include "stdio.h"
 #include "../../include/nmtype.h"
 #include "../../include/hadamard.h"
 
@@ -7,24 +8,25 @@ __attribute__((section(".data.imu0"))) long long array[size];
 __attribute__((section(".data.imu1"))) long long result[size];
 
 int main() {
-	srand((unsigned) 95);
-	for (int i = 0; i < size; i++) {
-		array[i] = ((long long)rand()<<32) | (rand());
+	array[0] = 0x8000000000000000;
+	for (int i = 1; i < size; i++)
+	{
+		array[i] = (array[i-1]<<4) + i;
 	}
 
 	int hash = 0;
 
-	for (int i = 8; i <= size; i+=8) {
+	for (int i = 8; i <= size*8; i+=8) {
 		nmppsAbs2_8s((nm8s*) array, (nm8s*) result, i);
 		hash ^= nmppsHash64u((long long *) result, i/8);
 	}
 
-	for (int i = 4; i <= size; i+=4) {
+	for (int i = 4; i <= size*4; i+=4) {
 		nmppsAbs2_16s((nm16s*) array, (nm16s*) result, i);
 		hash ^= nmppsHash64u((long long *) result, i/4);
 	}
 
-	for (int i = 2; i <= size; i+=2) {
+	for (int i = 2; i <= size*2; i+=2) {
 		nmppsAbs2_32s((nm32s*) array, (nm32s*) result, i);
 		hash ^= nmppsHash64u((long long *) result, i/2);
 	}
@@ -34,5 +36,9 @@ int main() {
 		hash ^= nmppsHash64u((long long *) result, i);
 	}
 
-	return hash^0xE3357C08;
+	int return_code = hash^0xd4b7b27f;
+
+	printf("return code = 0x%0x\n", return_code);
+
+	return return_code;
 } 
