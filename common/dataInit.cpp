83736fd19d034872cc825018a66a74220b8c9b8c
Alexander <mr.shutyak@list.ru>
2023-07-18 20:52:57 +0300

    header formalization

--- common/dataInit.cpp
+++ common/dataInit.cpp
@@ -14,30 +14,10 @@ extern "C" void makeShortArray(int* arrayPointer, int first, int last)
 	}
 }
 
-void fillArray(int* arrayPointer, int start, int end)
-{
-	int cnt = 0;
-	for(int i = start; i < end; i++)
-	{
-		*(arrayPointer + cnt) = i;
-		cnt++;
-	}
-}
-
 extern "C" void randArray(int* arrayPointer, int size)
 {
 	for(int i = 0; i < size; i++)
 	{
 		*(arrayPointer + i) = rand() ^ (rand() << 31);
-	}
-}
-
-void initMatrices(int* initMatrix, int* nullResultMatrix, int* nullResultMatrix_C, int height, int width)
-{
-	for(int i = 0; i < width * height; i++)
-	{
-		initMatrix[i] = rand();
-		nullResultMatrix[i] = 0;
-		nullResultMatrix_C[i] = 0;
 	}
 }
