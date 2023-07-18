1d1fad26d038c83a5d5af8326c9bb5a63cce0e0a
qt-bb <karpelevi4@gmail.com>
2023-07-18 17:24:25 +0300

    styling

--- dev-nastja/had/get_image_array.py
+++ dev-nastja/had/get_image_array.py
@@ -5,10 +5,8 @@ numpy.set_printoptions(threshold=sys.maxsize)
 
 img128 = imread('./marcie128.tif')
 # print(img128)
-
 numpy.savetxt("./image128.txt", img128, fmt='%d', delimiter=',', newline=',\n')
 
 img256 = imread('./marcie256.tif')
 # print(img256)
-
 numpy.savetxt("./image256.txt", img256, fmt='%d', delimiter=',', newline=',\n')
