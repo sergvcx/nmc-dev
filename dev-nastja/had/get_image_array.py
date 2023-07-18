6c1632156101be0ba234515a03b0c1f139197cba
qt-bb <karpelevi4@gmail.com>
2023-07-18 17:20:38 +0300

    generated image data for test

---
+++ dev-nastja/had/get_image_array.py
@@ -0,0 +1,14 @@
+from matplotlib.image import imread
+import sys
+import numpy
+numpy.set_printoptions(threshold=sys.maxsize)
+
+img128 = imread('./marcie128.tif')
+# print(img128)
+
+numpy.savetxt("./image128.txt", img128, fmt='%d', delimiter=',', newline=',\n')
+
+img256 = imread('./marcie256.tif')
+# print(img256)
+
+numpy.savetxt("./image256.txt", img256, fmt='%d', delimiter=',', newline=',\n')
