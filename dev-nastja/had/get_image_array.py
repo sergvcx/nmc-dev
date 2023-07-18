69170165950e343649a1c210d9f556c00447ef56
qt-bb <karpelevi4@gmail.com>
2023-07-18 20:11:17 +0300

    added image show

--- dev-nastja/had/get_image_array.py
+++ dev-nastja/had/get_image_array.py
@@ -1,16 +1,22 @@
-from matplotlib.image import imread
+import matplotlib.pyplot as plt
 import sys
 import numpy
 numpy.set_printoptions(threshold=sys.maxsize)
 
-img64 = imread('./marcie64.tif')
+img64 = plt.imread('./marcie64.tif')
 # print(img64)
 numpy.savetxt("./image64.txt", img64, fmt='%d', delimiter=',', newline=',\n')
 
-img128 = imread('./marcie128.tif')
+img128 = plt.imread('./marcie128.tif')
 # print(img128)
 numpy.savetxt("./image128.txt", img128, fmt='%d', delimiter=',', newline=',\n')
 
-img256 = imread('./marcie256.tif')
+img256 = plt.imread('./marcie256.tif')
 # print(img256)
 numpy.savetxt("./image256.txt", img256, fmt='%d', delimiter=',', newline=',\n')
+
+figure, axis = plt.subplots(1, 3)
+axis[0].imshow(img64, cmap='gray', vmin=0, vmax=255)
+axis[1].imshow(img128, cmap='gray', vmin=0, vmax=255)
+axis[2].imshow(img256, cmap='gray', vmin=0, vmax=255)
+plt.show()
