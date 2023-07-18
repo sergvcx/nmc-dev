67656a319ffc652bcbf94f3c99558a497aeeeaf2
qt-bb <karpelevi4@gmail.com>
2023-07-18 17:53:52 +0300

    added 64x64 pixels image

--- dev-nastja/had/get_image_array.py
+++ dev-nastja/had/get_image_array.py
@@ -3,6 +3,10 @@ import sys
 import numpy
 numpy.set_printoptions(threshold=sys.maxsize)
 
+img64 = imread('./marcie64.tif')
+# print(img64)
+numpy.savetxt("./image64.txt", img64, fmt='%d', delimiter=',', newline=',\n')
+
 img128 = imread('./marcie128.tif')
 # print(img128)
 numpy.savetxt("./image128.txt", img128, fmt='%d', delimiter=',', newline=',\n')
