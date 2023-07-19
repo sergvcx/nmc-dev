db7b10544524a8db481018548c89dd57a005fb93
qt-bb <karpelevi4@gmail.com>
2023-07-19 08:36:28 +0300

    changed txt format

--- dev-nastja/had/get_image_array.py
+++ dev-nastja/had/get_image_array.py
@@ -1,26 +1,17 @@
 import matplotlib.pyplot as plt
-import sys
 import numpy
-numpy.set_printoptions(threshold=sys.maxsize)
 
 img64 = plt.imread('./image_sources/marcie64.tif')
-# print(img64)
-numpy.savetxt("./image_sources_txt/image64.txt", img64, fmt='%d', delimiter=',', newline=',\n')
+numpy.savetxt("./image_sources_txt/image64.txt", img64, fmt='%d')
 
 img128 = plt.imread('./image_sources/marcie128.tif')
-# print(img128)
-numpy.savetxt("./image_sources_txt/image128.txt", img128, fmt='%d', delimiter=',', newline=',\n')
+numpy.savetxt("./image_sources_txt/image128.txt", img128, fmt='%d')
 
 img256 = plt.imread('./image_sources/marcie256.tif')
-# print(img256)
-numpy.savetxt("./image_sources_txt/image256.txt", img256, fmt='%d', delimiter=',', newline=',\n')
+numpy.savetxt("./image_sources_txt/image256.txt", img256, fmt='%d')
 
 figure, axis = plt.subplots(1, 3)
 axis[0].imshow(img64, cmap='gray', vmin=0, vmax=255)
 axis[1].imshow(img128, cmap='gray', vmin=0, vmax=255)
 axis[2].imshow(img256, cmap='gray', vmin=0, vmax=255)
-plt.show()
-
-interpolated_image = numpy.loadtxt("./interpolated_image.txt", dtype=int)
-plt.imshow(interpolated_image)
 plt.show()
