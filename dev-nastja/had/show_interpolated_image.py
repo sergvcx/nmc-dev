e59b500181bf5613b9dfdb1e598b7bc8568f5195
qt-bb <karpelevi4@gmail.com>
2023-07-19 08:37:36 +0300

    separated image generation and image show

---
+++ dev-nastja/had/show_interpolated_image.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy
+
+interpolated_image = numpy.loadtxt("./interpolated_image.txt", dtype=int)
+plt.imshow(interpolated_image, cmap='gray', vmin=0, vmax=255)
+plt.show()
