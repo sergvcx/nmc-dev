1463667b054450872adc7df22f6f727aab7656d2
qt-bb <karpelevi4@gmail.com>
2023-07-31 18:17:18 +0300

    fixes

--- test/merge/main.cpp
+++ test/merge/main.cpp
@@ -6,11 +6,12 @@
 #include "../../include/hadamard.h"
 
 const int srcSize = 128;
+const int dstSize = srcSize*4;
 __attribute__((section(".data.imu0"))) long long array0[srcSize/4];
 __attribute__((section(".data.imu0"))) long long array1[srcSize/4];
 __attribute__((section(".data.imu0"))) long long array2[srcSize/4];
 __attribute__((section(".data.imu0"))) long long array3[srcSize/4];
-__attribute__((section(".data.imu1"))) long long result[srcSize];
+__attribute__((section(".data.imu1"))) long long result[dstSize/4];
 
 int main() {
 	array0[0] = 0x0004000300020001;
@@ -24,23 +25,21 @@ int main() {
 		array3[i] = array3[i-1]+0x0001000100010001;
 	};
 
-	
 	int hash = 0;
 
-	for (int i = 4; i < srcSize; i+=4) {
+	for (int i = 4; i <= srcSize; i+=4) {
 		nmppsMerge4_16s((nm16s*) array0, (nm16s*) array1, (nm16s*) array2, (nm16s*) array3, (nm16s*) result, i);
-		hash ^= nmppsHash64u(result, i);
-	}
-	
+		hash ^= nmppsHash64u(result, i*16);
 	dump_64s("%llx ", array0, srcSize/4, 1, 1, 2);
 	dump_64s("%llx ", array1, srcSize/4, 1, 1, 2);
 	dump_64s("%llx ", array2, srcSize/4, 1, 1, 2);
 	dump_64s("%llx ", array3, srcSize/4, 1, 1, 2);
-	dump_64s("%llx ", result, srcSize/4, 4, 4, 2);
+	dump_64s("%llx ", result, dstSize/16, 4, 4, 2);
+	}
 	
 	int return_code = hash;
 
 	printf("return code = 0x%0x\n", return_code);
-
+	
 	return return_code;
 } 
