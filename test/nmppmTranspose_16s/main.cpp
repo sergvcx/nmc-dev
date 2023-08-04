66e8613b1272ec2f6c94c68f325b561f720cbc7e
qt-bb <karpelevi4@gmail.com>
2023-08-04 17:50:56 +0300

    columns transpose done

--- test/nmppmTranspose_16s/main.cpp
+++ test/nmppmTranspose_16s/main.cpp
@@ -5,8 +5,8 @@
 #include "../../include/mTranspose.h"
 #include "../../include/hadamard.h"
 
-const int width = 32;
-const int height = 8;
+const int width = 8;
+const int height = 32;
 __attribute__((section(".data.imu0"))) long long array[width * height / 4];
 __attribute__((section(".data.imu1"))) long long result[width * height / 4];
 
@@ -27,12 +27,18 @@ int main()
 			nmppmTranspose_16s((nm16s *)array, (nm16s *)result, i, j);
 			hash ^= nmppsHash64u(result, i * j / 4);
 
-			dump_16u("%04x ", array, i, j, j * 2, 2);
+			dump_16u("%04x ", array, i, j, j * 2, 1);
 			printf("\n");
-			dump_16u("%04x ", result, j, i, i * 2, 2);
+			dump_16u("%04x ", result, j, i, i * 2, 1);
 			printf("-----------------------------------\n");
 		}
 	}
+
+	// nmppmTranspose_16s((nm16s *)array, (nm16s *)result, height, width);
+	// dump_16u("%04x ", array, height, width, width * 2, 2);
+	// printf("\n");
+	// dump_16u("%04x ", result, width, height, height * 2, 2);
+	// printf("-----------------------------------\n");
 
 	// dump_64s("%0llx ", array, height, width / 4, width / 4, 2);
 	// printf("\n");
